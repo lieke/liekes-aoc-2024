1c14bc518d978e710e1103a8fce8507139cba163
Lieke <liekevdpoel@gmail.com>
2024-12-01 16:56:32 +0100

    adjustments for time measurement

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,11 +1,15 @@
 object Main:
 
   @main def adventOfCode2024(): Unit = {
-    var initTime = System.currentTimeMillis
     println("Welcome to Advent of Code 2024!")
     println("====Day1====")
+    var initTime = System.currentTimeMillis
     println("The sum of distances is: " + Day1.getDistance)
     println("The similarity score is: " + Day1.getSimilarityScore)
     var elapsedTime = System.currentTimeMillis-initTime
+    println("This took " + elapsedTime + " milliseconds")
+    println("====Day1====")
+    initTime = System.currentTimeMillis
+    elapsedTime = System.currentTimeMillis-initTime
     println("This took " + elapsedTime + " milliseconds")
   }
